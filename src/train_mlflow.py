b8801723942410b22384d3e85ad870aa2bb7d9e4
withonly_sujal <sujal.kadam20805@gmail.com>
2026-03-04 23:36:53 +0530

    Added MlFlow Training pipeline, Selected model is XG-Boost and added its registry

---
+++ src/train_mlflow.py
@@ -0,0 +1,86 @@
+# src/train_mlflow.py
+
+import pandas as pd
+import mlflow
+import mlflow.xgboost
+from pathlib import Path
+from xgboost import XGBClassifier
+from sklearn.metrics import accuracy_score, recall_score, f1_score, roc_auc_score
+from sklearn.model_selection import train_test_split
+
+
+# Paths to DS
+BASE_DIR = Path(__file__).resolve().parent.parent
+TRAIN_PATH = BASE_DIR / "data" / "processed" / "train_selected.csv"
+
+# Loading DS
+print("Loading dataset...")
+
+df = pd.read_csv(TRAIN_PATH)
+
+X = df.drop(columns=["label"])
+y = df["label"]
+
+
+# Train-Val Split
+X_train, X_val, y_train, y_val = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+
+# Starting MLflow Experiment
+mlflow.set_experiment("IDS_XGBoost_Experiment")
+
+with mlflow.start_run():
+
+    print("Training XGBoost model...")
+
+    model = XGBClassifier(
+        n_estimators=200,
+        max_depth=6,
+        learning_rate=0.1,
+        subsample=0.8,
+        colsample_bytree=0.8,
+        eval_metric="logloss",
+        random_state=42,
+        n_jobs=-1
+    )
+
+    model.fit(X_train, y_train)
+
+    # Predict
+    y_pred = model.predict(X_val)
+    y_prob = model.predict_proba(X_val)[:, 1]
+
+    # Eval Metrics
+    accuracy = accuracy_score(y_val, y_pred)
+    recall = recall_score(y_val, y_pred)
+    f1 = f1_score(y_val, y_pred)
+    roc_auc = roc_auc_score(y_val, y_prob)
+
+    print("Accuracy:", accuracy)
+    print("Recall:", recall)
+    print("F1:", f1)
+    print("ROC-AUC:", roc_auc)
+
+    # Log Parameters
+    mlflow.log_param("model_type", "XGBoost")
+    mlflow.log_param("n_estimators", 200)
+    mlflow.log_param("max_depth", 6)
+    mlflow.log_param("learning_rate", 0.1)
+
+    # Log Metrics
+    mlflow.log_metric("accuracy", accuracy)
+    mlflow.log_metric("recall", recall)
+    mlflow.log_metric("f1_score", f1)
+    mlflow.log_metric("roc_auc", roc_auc)
+
+
+# Model Logging
+    mlflow.xgboost.log_model(
+        model,
+        artifact_path="model",
+        registered_model_name="IDS_XGBoost_Model"
+    )
+
+print("Training complete. Model logged to MLflow.")
